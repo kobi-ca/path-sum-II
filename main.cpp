eb8e9745698e750f9d2d216eedbf77cc30680aec
Yacob (Kobi) Cohen-Arazi <kobic@qti.qualcomm.com>
2022-09-26 15:53:13 -0700

    working version
    where the tree is built

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@
 namespace {
     struct TreeNode final {
         int val{};
-        std::unique_ptr<TreeNode> left;
-        std::unique_ptr<TreeNode>  right;
+        std::shared_ptr<TreeNode> left;
+        std::shared_ptr<TreeNode>  right;
         TreeNode()  = default;
         explicit TreeNode(int x) : val(x) {}
         TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
@@ -29,145 +29,52 @@ namespace {
         }
     };
 
-    using uptr = std::unique_ptr<TreeNode>;
-
     class Solution {
     public:
-        std::vector <std::vector<int>> pathSum(std::unique_ptr<TreeNode> root, int targetSum) {
+        static std::vector <std::vector<int>> pathSum(TreeNode* root, const int targetSum) {
+            std::queue<TreeNode*> q;
+            q.push(root);
+            while(!q.empty()) {
+
+            }
             return {};
         }
     };
 
-    uptr get_node(const auto opt) {
-        if (opt) {
-            auto node = std::make_unique<TreeNode>(*opt);
-            std::clog << *opt << '\n';
-            return std::move(node);
-        } else {
-            std::clog << "nullptr" << '\n';
-            return nullptr;
-        }
-    }
-    using vec_of_tn_pairs = std::vector<std::pair<uptr, uptr>>;
-    vec_of_tn_pairs make_vector(std::initializer_list<std::optional<int>> l) {
-        vec_of_tn_pairs vec_of_pairs;
-        const auto beg = std::prev(rend(l));
-        for(auto iter = rbegin(l) ; iter != rend(l);) {
-            std::pair<uptr, uptr> p;
-            {
-                const auto opt = *iter;
-                auto n = get_node(opt);
-                if (iter == beg) {
-                    std::clog << "found begin\n";
-                    p.first = std::move(n);
-                    vec_of_pairs.push_back(std::move(p));
-                    break;
-                } else {
-                    p.second = std::move(n);
-                }
-            }
-            {
-                const auto opt = *(++iter);
-                p.first = get_node(opt);
-            }
-            vec_of_pairs.push_back(std::move(p));
-            ++iter;
-        }
-        return vec_of_pairs;
-    }
-
-    void print_item(const uptr& item) {
-        if (item) {
-            std::clog << *item;
-        } else {
-            std::clog << "null";
-        }
-    }
-
     void println() { std::clog << '\n'; }
     void printdelim( const char c = ',') { std::clog << c; }
 
-    uptr construct_tree(vec_of_tn_pairs& vec) {
-        std::clog << "\nconstruct tree\n";
-        if (vec.empty()) {
-            return {};
-        }
-        // first one is root which ->second can be ignored
-        auto& [node, _] = vec[0];
-        std::clog << node->val << '\n';
-        std::queue<uptr> queue;
-        for (auto&& iter = std::next(vec.begin()); iter != vec.end(); ++iter) {
-            auto&& [item1, item2] = *iter;
-            std::clog << "took: "; print_item(item1); printdelim() ; print_item(item2); println();
-
-            if (!queue.empty()) {
-                std::clog << "handling q\n";
-                auto item = std::move(queue.front());
-                std::clog << "item from q" << item->val << '\n';
-                if (item1) {
-                    item->left = std::move(item1);
-                    std::clog << "pushing to q " << item1->val << '\n';
-                    queue.push(std::move(item1));
-                }
-                if (item2) {
-                    item->right = std::move(item2);
-                    std::clog << "pushing to q " << item1->val << '\n';
-                    queue.push(std::move(item2));
-                }
-                queue.pop();
-                if (!queue.empty()) {
-                    continue;
-                }
-            }
-
-            std::clog << "after queue check\n";
-            if (item1) {
-                node->left = std::move(item1);
-                queue.push(std::move(item1));
-            }
-            if (item2) {
-                node->right = std::move(item2);
-                queue.push(std::move(item2));
-            }
-        }
-        return {};
-    }
-
-    void make_tree2(std::initializer_list<std::optional<int>> l) {
+    auto make_tree2(std::initializer_list<std::optional<int>> l) {
         auto iter = l.begin(); // root - assume root is there
         auto iter_unwrap = *iter;
         auto node = std::make_shared<TreeNode>(*iter_unwrap);
+        auto root = node;
         std::queue<std::shared_ptr<TreeNode>> q;
-        for(auto iter_opt = std::next(iter); iter_opt != l.end();) {
-            if (auto opt = *iter_opt; opt) {
-                std::clog << *opt << ' ';
-            } else {
-                std::clog << "nullptr" << ' ';
+        q.push(node);
+        auto iter_opt = std::next(iter);
+        while (!q.empty() && iter_opt != l.end()) {
+            node = q.front();
+            auto left_iter = iter_opt;
+            auto right_iter = std::next(iter_opt);
+            if (auto opt = *left_iter; opt) {
+                node->left = std::make_shared<TreeNode>(*opt);
             }
-            std::advance(iter_opt, 1);
-            if (auto opt = *iter_opt; opt) {
-                std::clog << *opt << '\n';
-            } else {
-                std::clog << "nullptr" << '\n';
+            if (auto opt = *right_iter; opt) {
+                node->right = std::make_shared<TreeNode>(*opt);
             }
-            std::advance(iter_opt, 1);
+            if (node->left)  q.push(node->left);
+            if (node->right) q.push(node->right);
+            std::advance(iter_opt, 2);
+            q.pop();
         }
+        return root;
     }
 
 }
 
 int main() {
-//    auto tree = make_vector({5, 4, 8, 11, std::nullopt, 13, 4, 7, 2, std::nullopt, std::nullopt, 5, 1});
-//    std::reverse(tree.begin(), tree.end());
-//    std::clog << "\nprinting tree:\n";
-//    for (const auto& [item1, item2] : tree) {
-//        print_item(item1);
-//        std::clog << ',';
-//        print_item(item2);
-//        std::clog << '\n';
-//    }
-//    const auto root = construct_tree(tree);
-
-    make_tree2({5, 4, 8, 11, std::nullopt, 13, 4, 7, 2, std::nullopt, std::nullopt, 5, 1});
+    auto root = make_tree2({5, 4, 8, 11, std::nullopt, 13, 4, 7, 2, std::nullopt, std::nullopt, 5, 1});
+    constexpr auto targetSum = 22;
+    auto vec = Solution::pathSum(root.get(), targetSum);
     return 0;
 }
